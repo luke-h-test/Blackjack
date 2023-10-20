03ff71a0a9124862477e5d2cbe740b369e76d5a7
luke-h-test <147532063+luke-h-test@users.noreply.github.com>
2023-10-20 16:23:00 +0100

    Add files via upload

--- src/main/java/com/lbg/cohort4/Main.java
+++ src/main/java/com/lbg/cohort4/Main.java
@@ -1,19 +1,62 @@
 package com.lbg.cohort4;
 
+import java.util.Scanner;
+
+import static java.lang.Integer.min;
+import static java.lang.Integer.parseInt;
+import static java.lang.Math.max;
+
+import java.lang.Math.*;
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    int[] userInput()
+    {
+        int[] userNums = new int[2];
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your first number: ");
+        userNums[0] = parseInt(sc.nextLine());
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+        System.out.println("Enter your second number: ");
+        userNums[1] = parseInt(sc.nextLine());
+
+        return userNums;
+
+    }
+
+    int comparator(int[] userChoices)
+    {
+        int checkOne = 21 - userChoices[0];
+        int checkTwo = 21 - userChoices[1];
+
+        if(checkOne >=0 & checkTwo >=0)
+        {
+            return max(userChoices[0], userChoices[1]);
         }
+        else if(checkOne >= 0 & checkTwo < 0)
+        {
+            return userChoices[0];
+        }
+        else if (checkOne < 0 & checkTwo >= 0)
+        {
+            return userChoices[1];
+        }
+        else {
+            return 0;
+        }
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        Main mm = new Main();
+        int[] userChoices = mm.userInput();
+
+        System.out.println(mm.comparator(userChoices));
+
     }
 }
